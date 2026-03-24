0a9502d99dcae8659abf033bbbbcd52b32d2afa0
David <qiwi@roundinternet.money>
2026-03-24 07:32:52 -0700

    event-json-lookups

--- gen/dex/ethereal/v1/event_json_value.go
+++ gen/dex/ethereal/v1/event_json_value.go
@@ -11,3 +11,15 @@ var EventType_json_value = map[string]EventType{
 	"OrderFill":             EventType_EVENT_TYPE_ORDER_FILL,
 	"TokenTransfer":         EventType_EVENT_TYPE_TOKEN_TRANSFER,
 }
+
+var EventType_json_name = map[EventType]string{
+	EventType_EVENT_TYPE_EVENT_UNSPECIFIED:      "EVENT_UNSPECIFIED",
+	EventType_EVENT_TYPE_L2_BOOK:                "L2Book",
+	EventType_EVENT_TYPE_TICKER:                 "Ticker",
+	EventType_EVENT_TYPE_TRADE_FILL:             "TradeFill",
+	EventType_EVENT_TYPE_SUBACCOUNT_LIQUIDATION: "SubaccountLiquidation",
+	EventType_EVENT_TYPE_POSITION_UPDATE:        "PositionUpdate",
+	EventType_EVENT_TYPE_ORDER_UPDATE:           "OrderUpdate",
+	EventType_EVENT_TYPE_ORDER_FILL:             "OrderFill",
+	EventType_EVENT_TYPE_TOKEN_TRANSFER:         "TokenTransfer",
+}
